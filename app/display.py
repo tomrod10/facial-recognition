f373d9b456ece0570be79d6fc825fbfea2a7cd24
Tomás <63356102+tomrod10@users.noreply.github.com>
2026-09-17 16:05:20 -0700

    set up opencv to capture video feed

---
+++ app/display.py
@@ -0,0 +1,28 @@
+import cv2
+
+
+class Display:
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)
+
+        if not self.cap.isOpened():
+            print("Cannot open camera")
+            exit()
+
+    def start(self):
+        while True:
+            ret, frame = self.cap.read()
+
+            if not ret:
+                print("Problem receiving frames...")
+                break
+
+            cv2.imshow("live feed", frame)
+
+            if cv2.waitKey(1) == ord("q"):
+                self.stop()
+                break
+
+    def stop(self):
+        self.cap.release()
+        cv2.destroyAllWindows()
